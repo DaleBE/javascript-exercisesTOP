d108795ab2c7c298802cef5094e975e82d12f1de
DaleBE <90635946+DaleBE@users.noreply.github.com>
2022-10-30 09:23:47 +0100

    completed exercises 03 and 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+   let reversedString = string.split('').reverse().join('');
+
+   return reversedString;
+   
 };
 
+console.log(reverseString('Doorbell juice'));
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, item) {
+
+   for (let p = 1; p <= arguments.length; p++) {
+
+      for (let i = 0; i <= array.length; i++) {
+            if (array[i] === arguments[p]) {
+
+                array.splice(i, 1);
+
+            }
+      };
+   
+   }
+
+   return array;
 
 };
+
+console.log(removeFromArray(['hello', 2, 3, 'world'], 3, 2, 'world'));
 
 // Do not edit below this line
 module.exports = removeFromArray;
