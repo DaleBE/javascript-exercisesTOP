c0ee4986571d8364aac1feccbeac964b264b0695
DaleBE <90635946+DaleBE@users.noreply.github.com>
2022-10-30 08:00:27 +0100

    completed 01 and 02 of the excerises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+const repeatString = function(string, numOfTimes) {
 
-};
+   let repeatString = [];
 
+   if (numOfTimes < 0) {
+
+      return 'ERROR';
+
+   } else {
+
+      for (let i = 0; i < numOfTimes; i++) {
+         repeatString.push(string);
+      }
+
+      return repeatString.join('');
+      
+   }
+}
 // Do not edit below this line
 module.exports = repeatString;
