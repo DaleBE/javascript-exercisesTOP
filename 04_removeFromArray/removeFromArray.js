b0ef9fd42642b4c488abaed03056f80d160205d6
DaleBE <90635946+DaleBE@users.noreply.github.com>
2022-10-30 10:12:15 +0100

    included an alternative solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -16,7 +16,28 @@ const removeFromArray = function(array, item) {
 
 };
 
-console.log(removeFromArray(['hello', 2, 3, 'world'], 3, 2, 'world'));
+// alternative way using rest parameters
 
+/*const removeFromArray = function(...args) {
+
+   //pull out array to work with
+   const array = args[0];
+
+   //define new array to hold the return items
+   const newArray = [];
+
+   // create an arrow fuction to run through each item in the array
+   array.forEach((item) => {
+      
+      // check that for the arguments passed and push only the unpassed arguments to the new array
+      if (!args.includes(item)) {
+         newArray.push(item);
+      }
+   });
+   //return array with new items
+   return newArray;
+};*/
+
+console.log(removeFromArray(['hello', 2, 3, 'world'],'' , 2, 'world'));
 // Do not edit below this line
 module.exports = removeFromArray;
