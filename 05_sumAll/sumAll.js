edea59d315aee3466bbdfc3e02c963b304b98916
DaleBE <90635946+DaleBE@users.noreply.github.com>
2022-11-03 10:02:00 +0100

    completed exercises  05, 06, and 07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
 
+   let startNum = firstNum;
+
+   let endNum = secondNum;
+
+   if (typeof firstNum != 'number' || typeof secondNum != 'number') {
+      return 'ERROR';
+   } else if (firstNum < 0 || secondNum < 0) {
+      return 'ERROR';
+   } else if (firstNum > secondNum) {
+      startNum = secondNum;
+      endNum = firstNum;
+   };
+
+    // variable to hold final sum
+   let finalSum = startNum;
+
+   // variable to hold next number to add
+   let nextNum = startNum;
+
+   while (startNum < endNum) {
+      nextNum++;
+      finalSum = finalSum + nextNum;
+      startNum++;
+   };
+
+   return finalSum;
 };
 
+console.log(sumAll(1, 123));
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,22 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+
+  let computed = (temp - 32) * (5 / 9);
+
+  return parseFloat(computed.toFixed(1)); 
 
 };
 
-const ctof = function() {
+const ctof = function(temp) {
+
+  let computed = temp * (9 / 5) + 32;
+
+  return parseFloat(computed.toFixed(1));
 
 };
 
+console.log(ftoc(32));
+console.log(ctof(-18));
+console.log(typeof ctof(0));
 // Do not edit below this line
 module.exports = {
   ftoc,
