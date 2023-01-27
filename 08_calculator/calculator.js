932d50869b4ef4dac75514e5d635d67fc7f9005f
DaleBE <90635946+DaleBE@users.noreply.github.com>
2023-01-27 13:26:16 +0100

    finished 08 and 09

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
+const add = function(num1, num2) {
+	return num1 + num2;
+};
+
+const subtract = function(num1, num2) {
+	return num1 - num2;
+};
+
+const sum = function(arrayIn) {
+
+   return arrayIn.reduce((total, index) => {
+      return total + index
+   }, 0)
 	
 };
 
-const subtract = function() {
-	
-};
+const multiply = function(arrayIn) {
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
+   return arrayIn.reduce((total, index) => {
+      return total * index
+   }, 1)
 
 };
 
-const power = function() {
+const power = function(root, exponent) {
 	
+   return root ** exponent
+
 };
 
-const factorial = function() {
+const factorial = function(numIn) {
 	
+   let factoralArray = [];
+
+   for (let i = 1; i <= numIn; i++) {
+      factoralArray.push(i)
+   };
+
+   return factoralArray.reduce((total, index) => {
+      return total * index
+   }, 1)
+
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,40 @@
-const palindromes = function () {
+const palindromes = function (inputString) {
+
+   let newString = inputString.replace(/ /g, '').toLowerCase();
+
+   let punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+
+   let cleanString = (newString) => {
+      return newString
+         .split('')
+         .filter(function(letter) {
+            return punctuation.indexOf(letter) === -1;
+         })
+         .join('');
+   }
+
+   const stringOne = cleanString(newString);
+   const stringTwo = cleanString(newString)
+                                       .split('')
+                                       .reverse()
+                                       .toString()
+                                       .replace(/,/g, '');
+
+   console.log(stringOne);
+   console.log(stringTwo);
+
+
+   if (stringOne === stringTwo) {
+      return true
+   } else {
+      return false
+   }
+
 
 };
+let inputString = 'A car, a man, a maraca.'
+console.log(palindromes(inputString))
+
 
 // Do not edit below this line
 module.exports = palindromes;
